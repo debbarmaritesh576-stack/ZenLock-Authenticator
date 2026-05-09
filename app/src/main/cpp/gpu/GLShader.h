438820d4a7987afe0106d2cd85bf96eea61296a8
Tab browser <debbarmaritesh576@gmail.com>
2026-05-09 16:49:28 +0530

    Update GLShader.h

--- app/src/main/cpp/gpu/GLShader.h
+++ app/src/main/cpp/gpu/GLShader.h
@@ -1,56 +1,68 @@
-#ifndef AEGIS_GL_SHADER_H
-#define AEGIS_GL_SHADER_H
+#include "GLShader.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
-#include <GLES3/gl3.h>
+GLShader::GLShader() : program(0), vertexShader(0), fragmentShader(0) {}
 
-class GLShader {
-public:
-    GLShader();
-    ~GLShader();
-
-    bool compile(const char* vertexSource, const char* fragmentSource);
-    void use();
-    
-    GLuint getProgram() const { return program; }
-    
-    void setUniform1i(const char* name, int value);
-    void setUniform1f(const char* name, float value);
-    void setUniformMatrix4fv(const char* name, const float* matrix);
-    
-private:
-    GLuint program;
-    GLuint vertexShader;
-    GLuint fragmentShader;
-    
-    GLuint compileShader(GLenum type, const char* source);
-    void checkCompileError(GLuint shader);
-};
-
-// PDF page vertex shader
-static const char* PAGE_VERTEX_SHADER = R"(
-#version 300 es
-in vec2 aPosition;
-in vec2 aTexCoord;
-out vec2 vTexCoord;
-uniform mat4 uMVP;
-
-void main() {
-    gl_Position = uMVP * vec4(aPosition, 0.0, 1.0);
-    vTexCoord = aTexCoord;
+GLShader::~GLShader() {
+    if (program) glDeleteProgram(program);
 }
-)";
 
-// PDF page fragment shader
-static const char* PAGE_FRAGMENT_SHADER = R"(
-#version 300 es
-precision mediump float;
-in vec2 vTexCoord;
-out vec4 fragColor;
-uniform sampler2D uTexture;
-
-void main() {
-    fragColor = texture(uTexture, vTexCoord);
+bool GLShader::compile(const char* vertexSource, const char* fragmentSource) {
+    vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+    if (!vertexShader) return false;
+    
+    fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+    if (!fragmentShader) return false;
+    
+    program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    
+    GLint linked;
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+    if (!linked) {
+        char log[512];
+        glGetProgramInfoLog(program, 512, nullptr, log);
+        __android_log_print(ANDROID_LOG_ERROR, "AegisGL", "Link error: %s", log);
+        return false;
+    }
+    
+    return true;
 }
-)";
 
-#endif
+GLuint GLShader::compileShader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    checkCompileError(shader);
+    return shader;
+}
+
+void GLShader::checkCompileError(GLuint shader) {
+    GLint compiled;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (!compiled) {
+        char log[512];
+        glGetShaderInfoLog(shader, 512, nullptr, log);
+        __android_log_print(ANDROID_LOG_ERROR, "AegisGL", "Compile error: %s", log);
+    }
+}
+
+void GLShader::use() {
+    glUseProgram(program);
+}
+
+void GLShader::setUniform1i(const char* name, int value) {
+    glUniform1i(glGetUniformLocation(program, name), value);
+}
+
+void GLShader::setUniform1f(const char* name, float value) {
+    glUniform1f(glGetUniformLocation(program, name), value);
+}
+
+void GLShader::setUniformMatrix4fv(const char* name, const float* matrix) {
+    glUniformMatrix4fv(glGetUniformLocation(program, name), 1, GL_FALSE, matrix);
+}
