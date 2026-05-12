010327e1824b8032f1b32ede2ad64ac041ab5824
Tab browser <debbarmaritesh576@gmail.com>
2026-05-12 08:24:13 +0530

    Update AegisPdfApp.kt

--- app/src/main/java/com/aegis/pdf/AegisPdfApp.kt
+++ app/src/main/java/com/aegis/pdf/AegisPdfApp.kt
@@ -1,7 +1,33 @@
-package com.aegis.pdf  
-  
-import android.app.Application  
-import dagger.hilt.android.HiltAndroidApp  
-  
-@HiltAndroidApp  
-class AegisPdfApp : Application()
+package com.aegis.pdf
+
+import android.app.Application
+import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
+import com.aegis.pdf.features.organizer.data.manager.TagManager
+import javax.inject.Inject
+
+@HiltAndroidApp
+class AegisPdfApp : Application() {
+
+    @Inject
+    lateinit var tagManager: TagManager
+
+    override fun onCreate() {
+        super.onCreate()
+        
+        Log.d("AegisPdfApp", "Application initialized")
+        
+        initializeTags()
+    }
+
+    private fun initializeTags() {
+        Thread {
+            try {
+                tagManager.initDefaultTags()
+                Log.d("AegisPdfApp", "Default tags initialized")
+            } catch (e: Exception) {
+                Log.e("AegisPdfApp", "Failed to initialize tags", e)
+            }
+        }.start()
+    }
+}
