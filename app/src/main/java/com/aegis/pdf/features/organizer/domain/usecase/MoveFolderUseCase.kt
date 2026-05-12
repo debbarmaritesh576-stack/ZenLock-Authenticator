4e496f2da50331804bc33f167dc7b6f18ca7a18f
Tab browser <debbarmaritesh576@gmail.com>
2026-05-12 08:13:51 +0530

    Create app/src/main/java/com/aegis/pdf/features/organizer/domain/usecase/MoveFolderUseCase.kt

---
+++ app/src/main/java/com/aegis/pdf/features/organizer/domain/usecase/MoveFolderUseCase.kt
@@ -0,0 +1,19 @@
+package com.aegis.pdf.features.organizer.domain.usecase
+
+import com.aegis.pdf.features.organizer.data.manager.FolderManager
+import com.aegis.pdf.features.organizer.domain.result.FolderResult
+import javax.inject.Inject
+
+class MoveFolderUseCase @Inject constructor(
+    private val folderManager: FolderManager
+) {
+    suspend operator fun invoke(
+        folderId: String,
+        targetFolderId: String?
+    ): FolderResult<Unit> {
+        if (folderId.isBlank()) {
+            return FolderResult.Error("Folder ID cannot be empty")
+        }
+        return folderManager.moveFolder(folderId, targetFolderId)
+    }
+}
