93f9baf0b3ebde848c4c5c7a085eaac0ca1a9032
Tab browser <debbarmaritesh576@gmail.com>
2026-05-17 13:35:59 +0530

    Update OtpRepository.kt

--- :core:auth/src/main/java/com/zenlock/auth/data/repository/OtpRepository.kt
+++ :core:auth/src/main/java/com/zenlock/auth/data/repository/OtpRepository.kt
@@ -1,20 +1,60 @@
 package com.zenlock.auth.data.repository
 
-import com.zenlock.auth.core.otp.TotpGenerator
-import com.zenlock.auth.data.model.OtpAccount
+import com.zenlock.auth.core.security.EncryptedOtpStorage
+import com.zenlock.auth.data.local.dao.AuthAccountDao
+import com.zenlock.auth.data.local.entity.AuthAccountEntity
+import com.zenlock.auth.domain.crypto.Base32Decoder
+import com.zenlock.auth.domain.crypto.TotpGenerator
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class OtpRepository @Inject constructor(
-    private val generator: TotpGenerator
+    private val dao: AuthAccountDao,
+    private val encryption: EncryptedOtpStorage,
+    private val base32Decoder: Base32Decoder,
+    private val totpGenerator: TotpGenerator
 ) {
 
-    fun getOtp(account: OtpAccount): String {
-        return generator.generateTOTP(account.secret)
+    /**
+     * Live OTP accounts stream (UI layer)
+     */
+    fun getAccounts(): Flow<List<AuthAccountEntity>> {
+        return dao.getAllAccounts()
     }
 
-    fun getRemainingTime(): Int {
-        return generator.getRemainingSeconds()
+    /**
+     * Generate current OTP for a single account
+     */
+    fun generateOtp(account: AuthAccountEntity): String {
+        return try {
+            val decryptedSecret = encryption.decrypt(account.encryptedSecret)
+            val secretBytes = base32Decoder.decode(decryptedSecret)
+
+            totpGenerator.generateTOTP(secretBytes)
+        } catch (e: Exception) {
+            "------"
+        }
+    }
+
+    /**
+     * Insert new account
+     */
+    suspend fun addAccount(account: AuthAccountEntity) {
+        dao.insertAccount(account)
+    }
+
+    /**
+     * Delete account
+     */
+    suspend fun deleteAccount(account: AuthAccountEntity) {
+        dao.deleteAccount(account)
+    }
+
+    /**
+     * Clear all accounts
+     */
+    suspend fun clearAll() {
+        dao.clearAll()
     }
 }
